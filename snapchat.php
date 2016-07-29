0986eb80a59cacf59e7b85ebfb9f46dedec5093e
Stephen Ritchie <stephenfritchie@gmail.com>
2016-07-29 17:55:19 -0400

    Add files via upload

---
+++ snapchat.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>The Draw of Snapchat-Stephen Ritchie Designs</title>
+  <link rel="stylesheet" type="text/css" href="../styles.css">
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+  <script src="js/scrollTo.js"></script>
+  <script src="js/floatingNav.js"></script>
+  <script src="js/skrollr.js"></script>
+</head>
+<body background="../media/code.png">
+
+
+  <div class="wrapper">
+
+    <!-- HEADER -->
+    <?php require '../header.html'; ?>
+
+
+    <!-- CONTENT -->
+    <div class="content">
+    
+      <div id="resume">
+        <br>
+        <br>
+        <br>
+        <h2>The Draw of Snapchat</h2>
+        <hr>
+        <p>Snapchat has taken the social media world by storm, and there doesn't seem to be any end in sight...</p>
+        <p>Snapchat is a mobile application that was first released in late 2011. The basic premise is quite simple, you can send pictures to other Snapchat users that disappear after a set amount of time, often no more than ten seconds.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus lobortis tristique. Nunc sollicitudin iaculis ipsum, non maximus felis rutrum sit amet. Praesent commodo vitae enim eget imperdiet. Integer vel ipsum eget dolor pellentesque ultricies a et sem. Nullam malesuada iaculis diam, vel elementum sapien blandit in. Cras sollicitudin sem eu erat rhoncus, in tempor nisl aliquam. Curabitur tempus velit id dui rhoncus gravida.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      </div>
+
+      
+
+    <!-- FOOTER -->
+    <?php require '../footer.html'; ?>
+
+    
+
+  </div>
+</body>
+</html>
+
+
+
