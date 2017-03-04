335967851187594e247bafe68c50ea36632c1c7c
recklesspanda <stephenfritchie@gmail.com>
2017-03-04 16:57:18 -0500

    rehaul

---
+++ php/contactme.php
@@ -0,0 +1,13 @@
+<?php
+
+$DEBUG = True;
+
+if ($DEBUG)
+{
+  echo "<pre>__DEBUGGING__</pre>";
+  echo "<pre>name:".    $_POST['name']    ."</pre>";
+  echo "<pre>email:".   $_POST['email']   ."</pre>";
+  echo "<pre>message:". $_POST['message'] ."</pre>";
+}
+
+ ?>
